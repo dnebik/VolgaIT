17306ab17a956691d43f6777f8b8c8781b16cf5c
ya.dneb@yandex.ru <ya.dneb@yandex.ru>
2020-10-09 15:18:33 +0300

    readme настройка базы

--- config/db.php
+++ config/db.php
@@ -3,12 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=volga_it',
-    'username' => 'dneb',
-    'password' => '241978b',
+    'username' => '???',
+    'password' => '???',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
