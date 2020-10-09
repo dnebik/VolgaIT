ccc7b773db5544ba50231ff49986749a3b2d9d2c
ya.dneb@yandex.ru <ya.dneb@yandex.ru>
2020-10-09 15:36:00 +0300

    состав заказа

--- views/admin/view.php
+++ views/admin/view.php
@@ -37,4 +37,34 @@ $this->params['breadcrumbs'][] = $this->title;
         ],
     ]) ?>
 
+    <hr>
+
+    <h3>Состав заказа</h3>
+
+    <table class="table table-hover table-striped">
+        <thead>
+        <tr>
+            <th style="padding: 8px; border: 1px solid #ddd">Наименование</th>
+            <th style="padding: 8px; border: 1px solid #ddd">Количество</th>
+            <th style="padding: 8px; border: 1px solid #ddd">Цена</th>
+            <th style="padding: 8px; border: 1px solid #ddd">Сумма</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        <?
+        $goods = $model->orderGoods;
+        foreach ($goods as $good) {
+            ?>
+            <tr>
+                <td style="padding: 8px; border: 1px solid #ddd"><?= $good['name'] ?></td>
+                <td style="padding: 8px; border: 1px solid #ddd"><?= $good['quantity'] ?></td>
+                <td style="padding: 8px; border: 1px solid #ddd"><?= $good['price'] ?> рублей</td>
+                <td style="padding: 8px; border: 1px solid #ddd"><?= $good['price'] * $good['quantity'] ?> рублей</td>
+            </tr>
+        <? } ?>
+
+        </tbody>
+    </table>
+
 </div>
